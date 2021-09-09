e339c982b5fa50f36463cf9305afdd963f6663d8
Jack McNeil <mcneil.jack.m@gmail.com>
2021-09-09 14:01:10 -0400

    ratings create and list item

---
+++ MovieRater.Models/RatingsCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Models
+{
+    public class RatingsCreate
+    {
+        [Required]
+        [Range(0.0, 5.0)]
+        public double Rating { get; set; }
+        [MinLength(2, ErrorMessage = "Comment is too short please enter at least 2 chracters")]
+        public string Comment { get; set; }
+
+        public int MovieId { get; set; }
+        public int ShowId { get; set; }
+    }
+}
