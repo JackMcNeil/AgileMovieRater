d52312f106fb3ae554498fabebc5f787daaa2cbe
John Mai <Maijohn960@gmail.com>
2021-09-09 10:56:01 -0400

    First Commit

---
+++ MovieRater.Data/Show.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Data
+{
+    public class Show
+    {
+        [Required]
+        public  string Title  { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string  Rating  { get; set; }
+        [Required]
+        public int Season { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
